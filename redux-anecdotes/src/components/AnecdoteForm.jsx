358b8649baa9c717b5731b53b6185530a0b7567d
Yakumaa <maharjanshrish8@gmail.com>
2026-06-28 08:21:42 +0545

    6.6-6.8: Add AnecdoteList and AnecdoteForm components for displaying and creating anecdotes

--- redux-anecdotes/src/components/AnecdoteForm.jsx
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 
-const NewAnecdote = () => {
+const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
   const addAnecdote = async (event) => {
@@ -24,4 +24,4 @@ const NewAnecdote = () => {
   )
 }
 
-export default NewAnecdote
+export default AnecdoteForm
--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
-const Anecdotes = () => {
+const AnecdoteList = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector((state) => state)
 
@@ -23,4 +23,4 @@ const Anecdotes = () => {
   )
 }
 
-export default Anecdotes
+export default AnecdoteList
