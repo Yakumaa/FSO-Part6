60576aa6ea784807fc5ef1010e4ca5fee331c1d1
Yakumaa <maharjanshrish8@gmail.com>
2026-06-28 08:15:30 +0545

    6.5: Sort anecdotes by votes in Anecdotes component

--- redux-anecdotes/src/components/Anecdotes.jsx
+++ redux-anecdotes/src/components/Anecdotes.jsx
@@ -5,10 +5,12 @@ const Anecdotes = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector((state) => state)
 
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
